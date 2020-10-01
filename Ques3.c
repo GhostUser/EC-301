5cd14f0fe049d68a61232313b8081f6c78d569a5
GhostUser <35362972+GhostUser@users.noreply.github.com>
2020-10-01 11:42:49 +0530

    Ques 3

---
+++ Ques3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/* written on 28th sept*/
+int main()
+{
+    char a;
+    printf("Enter a uppercase char: ");
+    scanf("%c", &a);
+    printf("The lowercase char is %c", a+32);
+	return 0;
+}
