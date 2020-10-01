11da1c969110edb808b065f18ced4344be8b220e
Rahul Raj <35362972+GhostUser@users.noreply.github.com>
2020-10-01 11:36:14 +0530

    Ques 2

---
+++ Ques2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int x,y;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &x, &y);
+    printf("The sum of the numbers is %d\n", x+y);
+    printf("The difference of the numbers is %d\n", x-y);
+    printf("The product of the numbers is %d\n", x*y);
+    printf("The division of both numbers is %f\n",(float) x/y);
+    printf("The modulo of both numbers is %d\n", x%y);
+	return 0;
+}
