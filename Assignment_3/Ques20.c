8de775ebe60f9f34776f0ed8a6faed1dd48156ff
GhostUser <35362972+GhostUser@users.noreply.github.com>
2020-11-02 21:03:43 +0530

    Updated the ques 20

--- Assignment_3/Ques20.c
+++ Assignment_3/Ques20.c
@@ -3,7 +3,7 @@ Q20.Write a C program to find factorial of a
 number entered by user.
 */
 #include <stdio.h>
-
+/*
 int factorial(int n){
     if (n<=1){
         return 1;
@@ -11,12 +11,16 @@ int factorial(int n){
         return n*factorial(n-1);
     }
 }
+*/
 
 int main(){
     int n, result;
     printf("Enter a number: ");
     scanf("%d", &n);
-    result=factorial(n);
+    result=1;
+    for (int i=1; i<n+1; i++){
+        result=result*i;
+    }
     printf("The factorial of %d is: %d", n, result);
     return 0;
 }
