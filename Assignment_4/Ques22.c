df1dccd08cacc70903577e496719394d254643eb
GhostUser <35362972+GhostUser@users.noreply.github.com>
2020-11-02 21:22:28 +0530

    Added ques 22

--- Assignment_4/Ques22.c
+++ Assignment_4/Ques22.c
@@ -8,13 +8,13 @@ number.
 #include <math.h>
 
 int main(){
-    int n, p,rem, bi=0, i=0;
+    int dec, p,rem, bi=0, i=0;
     printf("Enter a number: ");
-    scanf("%d", &n);
-    p=n;
-    while(n!=0){
-        rem=n%2;
-        n=n/2;
+    scanf("%d", &dec);
+    p=dec;
+    while(dec!=0){
+        rem=dec%2;
+        dec=dec/2;
         bi=bi+rem*pow(10,i);
         i++;
     }
