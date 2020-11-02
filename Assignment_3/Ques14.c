d4fca817b51d6858e1a695eb29d18080031d55a0
GhostUser <35362972+GhostUser@users.noreply.github.com>
2020-11-02 21:07:51 +0530

    Updated the ques 14

--- Assignment_3/Ques14.c
+++ Assignment_3/Ques14.c
@@ -4,18 +4,25 @@ power y without using build in function pow().
 */
 
 #include <stdio.h>
-
+/*
 int power(int x,int y){
     if (y==0){
         return 1;
     }
     return x*power(x,y-1);
 }
+*/
+
 int main(){
-    int x, y, r=1, i=1;
-    printf("Enter a number x and raiised power y: ");
+    int x, y, r=1,i;
+    printf("Enter a number x and raised power y: ");
     scanf("%d %d", &x, &y);
-    r=power(x,y);
+    r=1;
+    i=y;
+    while(i>0){
+        r=r*x;
+        i=i-1;
+    }
     printf("%d raised to power %d is %d", x,y,r);
     return 0;
 }
